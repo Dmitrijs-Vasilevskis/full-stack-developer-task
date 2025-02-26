19a07b23b72f652bffbca565fd5f7f1e8c390b1f
Dmitrijs-Vasilevskis <dmitrij.vasilevski@gmail.com>
2025-02-26 17:19:21 +0200

    test of api url

--- frontend/src/services/graphql.tsx
+++ frontend/src/services/graphql.tsx
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const API_URL =
+  "https://cors-anywhere.herokuapp.com/http://scandiweb-test.42web.io/graphql";
+  
 const client = new ApolloClient({
-  uri: "http://scandiweb-test.42web.io//graphql",
+  uri: API_URL,
   cache: new InMemoryCache(),
 });
 
