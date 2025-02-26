419ed66910d97c520042c2a31bae600ead5ffe76
Dmitrijs-Vasilevskis <dmitrij.vasilevski@gmail.com>
2025-02-26 19:30:17 +0200

    remove loggers

--- public/index.php
+++ public/index.php
@@ -2,10 +2,6 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
