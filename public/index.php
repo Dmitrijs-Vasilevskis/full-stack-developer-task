005f992b2dbc74f084e7c9297f16459b60df2dbc
Dmitrijs-Vasilevskis <dmitrij.vasilevski@gmail.com>
2025-02-26 18:46:50 +0200

    index.php error display

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-use App\Models\Products\Product;
-use App\Database\Database;
-use App\Models\Category\Categories;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
