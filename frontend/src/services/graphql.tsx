f94d14ec185684e0eb579bf2f7fa229827d2f407
Dmitrijs-Vasilevskis <dmitrij.vasilevski@gmail.com>
2025-02-26 21:36:16 +0200

    check

--- frontend/src/services/graphql.tsx
+++ frontend/src/services/graphql.tsx
@@ -1,11 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const API_URL =
-  "https://lightgreen-porcupine-187566.hostingersite.com/graphql";
+  "https://lightgreen-porcupine-187566.hostingersite.com/graphql/";
 
 const client = new ApolloClient({
   uri: API_URL,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache()
 });
 
 export default client;
